68c49420d6646dd9e7338680bce9880711eccbec
seekdestiny <jifeiqian@gmail.com>
2020-02-17 15:54:59 -0800

    exercise 1

--- src/environment.cpp
+++ src/environment.cpp
@@ -42,12 +42,16 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     // ----------------------------------------------------
     
     // RENDER OPTIONS
-    bool renderScene = true;
+    bool renderScene = false;
     std::vector<Car> cars = initHighway(renderScene, viewer);
     
     // TODO:: Create lidar sensor 
-
+    std::shared_ptr<Lidar> lidar = std::make_shared<Lidar>(cars, 0);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr inputCloud = lidar->scan();
+    //renderRays(viewer, lidar->position, inputCloud);
+    renderPointCloud(viewer, inputCloud, "inputCloud");
     // TODO:: Create point processor
+    std::shared_ptr<ProcessPointClouds<pcl::PointXYZ>> pointProcessor(new ProcessPointClouds<pcl::PointXYZ>()); 
   
 }
 
--- src/processPointClouds.cpp
+++ src/processPointClouds.cpp
@@ -53,7 +53,7 @@ std::pair<typename pcl::PointCloud<PointT>::Ptr, typename pcl::PointCloud<PointT
 {
     // Time segmentation process
     auto startTime = std::chrono::steady_clock::now();
-	pcl::PointIndices::Ptr inliers;
+    pcl::PointIndices::Ptr inliers;
     // TODO:: Fill in this function to find inliers for the cloud.
 
     auto endTime = std::chrono::steady_clock::now();
