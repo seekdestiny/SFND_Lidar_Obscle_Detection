f684418ce260cb7cd32580cb301cd925fa357b08
seekdestiny <jifeiqian@gmail.com>
2020-02-19 21:38:01 -0800

    exercise 3

--- src/quiz/cluster/cluster.cpp
+++ src/quiz/cluster/cluster.cpp
@@ -75,15 +75,41 @@ void render2DTree(Node* node, pcl::visualization::PCLVisualizer::Ptr& viewer, Bo
 
 }
 
+void clusterHelper(std::vector<int>& cluster, std::vector<bool>& processed, const std::vector<std::vector<float>>& points, int indice,
+                   KdTree* tree, float distanceTol) {
+   processed[indice] = true;
+   cluster.push_back(indice);
+     
+   std::vector<int> ids = tree->search(points[indice], distanceTol);
+   for (int id : ids) {
+    if (!processed[id]) {
+      clusterHelper(cluster, processed, points, id, tree, distanceTol);
+    }
+   }
+}
+
 std::vector<std::vector<int>> euclideanCluster(const std::vector<std::vector<float>>& points, KdTree* tree, float distanceTol)
 {
 
 	// TODO: Fill out this function to return list of indices for each cluster
 
 	std::vector<std::vector<int>> clusters;
- 
-	return clusters;
+  std::vector<bool> processed(points.size(), false);
 
+  int i = 0;
+  while (i < points.size()) {
+    if (processed[i]) {
+      i++;
+      continue;
+    }
+
+    std::vector<int> cluster;
+    clusterHelper(cluster, processed, points, i, tree, distanceTol);
+    clusters.push_back(cluster);
+    i++;
+  }
+
+	return clusters;
 }
 
 int main ()
--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -29,13 +29,56 @@ struct KdTree
 	{
 		// TODO: Fill in this function to insert a new point into the tree
 		// the function should create a new node and place correctly with in the root 
+    if (root == NULL) {
+      root = new Node(point, id);
+      return;
+    }
 
+    int depth = 0;
+    Node* cur = root;
+    while (cur->id != id) {
+      if (cur->point[depth % 2] <= point[depth % 2]) {
+        if (cur->right == NULL) {
+          cur->right = new Node(point, id);
+        }
+        cur = cur->right;
+        depth++;
+      } else {
+        if (cur->left == NULL) {
+          cur->left = new Node(point, id);
+        }
+        cur = cur->left;
+        depth++;
+      } 
+    }
 	}
+
+  void searchHelper(std::vector<int>& ids, std::vector<float> target, float distanceTol, int depth, Node* node) {
+    if (node == NULL) return;
+      
+    if (node->point[0] >= target[0] - distanceTol &&
+        node->point[0] <= target[0] + distanceTol &&
+        node->point[1] >= target[1] - distanceTol &&
+        node->point[1] <= target[1] + distanceTol) {
+        float distance = sqrt((node->point[0] - target[0]) * (node->point[0] - target[0]) + (node->point[1] - target[1]) * (node->point[1] - target[1]));
+        if (distance <= distanceTol) {
+          ids.push_back(node->id);
+        }
+    }
+
+    if (target[depth % 2] - distanceTol < node->point[depth % 2]) {
+      searchHelper(ids, target, distanceTol, depth + 1, node->left);
+    }
+    if (target[depth % 2] + distanceTol > node->point[depth % 2])  {
+      searchHelper(ids, target, distanceTol, depth + 1, node->right);
+    }
+  }
 
 	// return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
 		std::vector<int> ids;
+    searchHelper(ids, target, distanceTol, 0, root);
 		return ids;
 	}
 	
